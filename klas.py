82d3b1d2b36be1018100f8a95694032ec8e4d076
Boyard-zzz <ammaarmakame2008@gmail.com>
2025-10-10 09:36:58 +0200

    Add files via upload

---
+++ klas.py
@@ -0,0 +1,11 @@
+import random
+
+klas25B = ['Raad', 'Josip', 'Roan', 'Cris', 'Kourosh', 'Sven F.', 'Anas', 'Jeffry', 'Atilla', 'Ammaar', 'Ilia', 'Lion', 'Orhan', 'Daniël', 'Armin', 'Jovan', 'Ward', 'Stefan', 'Caelan', 'Noah', 'Kaydeon', 'Sven W.', 'Phoenix']
+teller = 1
+while teller <= len(klas25B):
+ number = int(0)
+ int(input("Choose team":))
+ (klas25B[random.randint(number, len(klas25B)-1)])
+ teller+=1
+
+ 
